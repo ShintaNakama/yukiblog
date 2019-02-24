9f1623a3ce2f33a2971c4c7bedad5180be3b5892
ShintaNakama <shintanakama77@gmail.com>
2019-02-25 02:04:32 +0900

    Fix Gemfile, deploy

--- app-start.sh
+++ app-start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+cd /var/www/rails
+sudo chown -R ec2-user:wheel yukiblog
 cd /var/www/rails/yukiblog
 # latest=`sudo ls -1t * | head -1`
 bundle install --path vendor/bundle
 bundle exec rake assets:precompile RAILS_ENV=production
 # bundle exec rails db:migrate
 # sudo cp ${latest} /var/www/rails/current
-sudo ln -sf /var/www/rails/yukiblog /var/www/html/yukiblog
+sudo ln -sf /var/www/rails/yukiblog /var/www/html
 sudo service httpd graceful
