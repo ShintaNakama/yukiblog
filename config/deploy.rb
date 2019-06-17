c5bec817c553e7d4c82be2a15abeeb96733e91d0
ShintaNakama <shintanakama77@gmail.com>
2019-06-18 01:56:47 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,8 @@ set :deploy_to, "/var/www/yukiblog"
 
 # Default value for :linked_files is []
 
-# append :linked_files, "config/master.key"
+append :linked_files, "config/master.key"
+# set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp", ".bundle"
