ca43b1d090f0c9c4d9a147fc8ad44d1a0fc8aaba
ShintaNakama <shintanakama77@gmail.com>
2019-06-23 02:35:54 +0900

    puma設定編集7

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :user, "nakamashinta"
 # # Default value for :linked_files is []
 
 # # append :linked_files, "config/master.key"
-set :linked_files, fetch(:linked_files, []).push("config/master.key") 
+# set :linked_files, fetch(:linked_files, []).push("config/master.key") 
 
 # # Default value for linked_dirs is []
 # # append :linked_dirs, "log", "tmp", ".bundle"
