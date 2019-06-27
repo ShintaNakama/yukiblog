4128926bd648f69e9a1ac9a66415f5cdab559f2f
ShintaNakama <shintanakama77@gmail.com>
2019-06-28 00:25:06 +0900

    webサーバ追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -25,6 +25,7 @@ set :branch, 'master'
 
 # server "35.243.71.228", user: "nakamashinta", roles: %w{app web}, other_property: :other_value
 server "35.243.71.228", user: "nakamashinta", roles: %w{web app}
+server "35.221.120.227", user: "nakamashinta", roles: %w{web app}
 
 # server "10.45.128.3 ", user: "root", roles: %w{db}
 
